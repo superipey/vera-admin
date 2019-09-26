0f2f7ef4b817bb9adb3406855ccf4a8e236b1aa9
Ferry Stephanus Suwita <ferry.stephanus.s@gmail.com>
2019-09-26 14:06:02 +0700

    deploy favicon

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -80,7 +80,15 @@
     <link href="{{ asset('assets') }}/css/demo1/skins/aside/dark.css" rel="stylesheet" type="text/css" />
 
     <!--end::Layout Skins -->
-    <link rel="shortcut icon" href="{{ asset('assets') }}/media/logos/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicon') }}/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon') }}/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon') }}/favicon-16x16.png">
+    <link rel="manifest" href="{{ asset('assets/images/favicon') }}/site.webmanifest">
+    <link rel="mask-icon" href="{{ asset('assets/images/favicon') }}/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon') }}/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="{{ asset('assets/images/favicon') }}/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 
     @stack('style')
 </head>
